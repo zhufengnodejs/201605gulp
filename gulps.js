0cb01fbf45e9b1fa3ce64c32a13271e390594164
zhufengnodejs <zhufengnodejs@126.com>
2016-08-11 15:37:33 +0800

    任务的依赖和异步任务的处理

--- gulps.js
+++ gulps.js
@@ -4,25 +4,28 @@ var gulp = {
         gulp.tasks.push(fn);
     },
     start:function(){//开始执行所有的任务
-       /* gulp.tasks.forEach(function(task){
-            task();
-        });*/
         var index = 0;
         //每调用一次callback就相当于取出当前函数并执行
-        function callback(){
+        function next(){
+            if(index >= gulp.tasks.length){
+                return;
+            }
             var task = gulp.tasks[index++];//取出当前任务
-
+            task(next);
         }
-        callback();
+        next();
     }
 }
-gulp.task(function(callback){
+gulp.task(function(next){
     setTimeout(function(){
         console.log(1);
-        callback();
+        next();
     },1000)
 });
-gulp.task(function(){
-    console.log(2);
+gulp.task(function(next){
+    setTimeout(function(){
+        console.log(2);
+        next();
+    },1000)
 });
 gulp.start();
