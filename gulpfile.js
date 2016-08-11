1d3eea2ade04c47ff4f9d65cc292788963f5ad6a
zhufengnodejs <zhufengnodejs@126.com>
2016-08-11 15:20:48 +0800

    任务的依赖和异步任务的处理

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require('gulp');
-var util = require('util');
-//把src下面的所有的文件都拷贝到build目录下
-gulp.task('default',function() {
-    //生成的目录=dest目录+通配符开始匹配的目录
-    // base 是指定基准目录
-    //如果指定的base
-    //生成的目录=dest目录+(源文件的完整目录-base路径)
-    gulp.src(['./src/html/*.html','./src/javascript/*.js'],{base:'./src'})
-        .pipe(gulp.dest('./build'));
+//当一个任务依赖的任务是异步的时候，那么当前任务并不会等待异步任务完成
+gulp.task('买米',function(callback){
+    setTimeout(function(){
+        console.log('买米');
+        //如果调了callback就表示此异步执行完毕，
+        //如果不调callback就表示此异步一直没有结束，后续任务无法得到执行
+        callback();
+    },2000)
 });
-/*gulp.task('copy-imgs',function() {
-    gulp.src('./src/imgs/!*.@(jpg|png)').pipe(gulp.dest('./build'))
-});*/
-gulp.task('copy-imgs',function() {
-    gulp.src('./src/imgs/*.{jpg,png}').pipe(gulp.dest('./build'))
+gulp.task('买菜',function(callback){
+    setTimeout(function(){
+        console.log('买菜');
+        callback();
+    },4000)
+});
+
+gulp.task('做饭',['买米','买菜'],function(){
+    console.log('做饭');
+});
+//如果执行任务的时候不加任务名，gulp会自动寻找名字叫default的任务
+gulp.task('吃饭',['做饭'],function(){
+    console.log('吃饭');
 });
