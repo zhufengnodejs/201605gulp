f7009ba6356fe08f4458cdd206dfc0fab124b364
zhufengnodejs <zhufengnodejs@126.com>
2016-08-11 16:24:37 +0800

    使用less插件

--- gulp-load-plugins.js
+++ gulp-load-plugins.js
@@ -1,7 +1,7 @@
 module.exports = function(){
     /**
      * 1.初始化一个空对象$
-     * 2. 读取package.json文件，得到一个json
+     * 2. 读取package.json文件，得到一个json, require('./package.json');
      * 3. 取得devDependencies属性对象
      * 4.寻找此属性对象以gulp-开头的属性
      * 5. 加载这些gulp插件模块，把返回值赋给$对象,属性名就是插件名减去gulp-
